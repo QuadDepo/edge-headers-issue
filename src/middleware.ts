fd66a86d8d7955caf4615affe17cde690573b60f
Florentin Eckl <ecklf@icloud.com>
2024-09-11 09:11:22 +0200

    fix middleware usage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,25 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-  ],
-}
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         */
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    ],
+};
 
- 
-export function middleware(req: NextRequest) {
-
-    req.headers.set('x-test', 'foo-bar');
-    
-    const response = NextResponse.next({
-        headers: {
-            'x-foo': 'foo-bar'
-        }
+export function middleware(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("x-test", "foo-bar");
+    return NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        },
     });
-
-    response.headers.set('x-test', 'foo-bar');
-
-    return response;
 }
